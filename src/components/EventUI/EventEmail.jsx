f79b96d78ab5f041ff4a87227efb525116bfcc85
Christopher Parsons <chris.p@madetech.com>
2021-11-26 12:00:38 +0000

    Add Email notification to EventEmail page

--- src/components/EventUI/EventEmail.jsx
+++ src/components/EventUI/EventEmail.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import EmailIcon from '@mui/icons-material/Email';
+import { Badge } from "@mui/material";
 
 const EventEmail = ({ event }) => {
-  const emailAddress = `${event.characterName.replace(" ", ".").toLowerCase()}@notspam.net`
+  const emailAddress = `${event.characterName
+    .replace(" ", ".")
+    .toLowerCase()}@notspam.net`;
   return (
     <div>
+      <Badge badgeContent={1} color="primary">
+        <EmailIcon color="action" />
+      </Badge>
       <h3>Subject: {event.name}</h3>
       <h4>From: {emailAddress}</h4>
       <p>{event.description}</p>
