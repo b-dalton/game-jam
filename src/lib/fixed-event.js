5d491027cd02da83de89d8fad520c4e073c8a581
b-dalton <ben.dalton@madetech.com>
2021-11-26 16:23:08 +0000

    removed unused events

--- src/lib/fixed-event.js
+++ src/lib/fixed-event.js
@@ -1,22 +1,5 @@
 export const getFixedEvent = (state) => {
   const fixedEvents = [
-    // {
-    //   name: `${state.companyName} floats on the stock market`,
-    //   characterName: "@financial-grimes",
-    //   description: `${state.companyName} has floated on the stock market today, evidenced by their executive team seen stumbling around the London Stock Exchange at 8:30am blind drunk. ${state.companyName} has netted a cool $420,000 as part of the float.`,
-    //   type: "clackerNews",
-    //   condition: ({ gameTime }) => {
-    //     return gameTime === 180;
-    //   },
-    //   joke: "",
-    //   image: "coffee",
-    //   action: (state) => {
-    //     return {
-    //       ...state,
-    //       currency: state.currency + 420000,
-    //     };
-    //   },
-    // },
     {
       name: "Buyout or IPO",
       characterName: "Bought Works",
@@ -42,25 +25,6 @@ export const getFixedEvent = (state) => {
         };
       },
     },
-    // {
-    //   name: "Would you like to pivot the company into Crypto?",
-    //   characterName: "Pivot into Crypto",
-    //   description: "Crypto currencies are becoming increasingly popular. Tempted to get on the Crypto train?",
-    //   type: "choice",
-    //   condition: (state) => state.gameTime === 10000,
-    //   action: (state, choice) => {
-    //     if (choice === "Yes")
-    //       return {
-    //         ...state,
-    //         currency: state.currency - 50000,
-    //         employeeHappiness: state.employeeHappiness - 10
-    //       };
-    //     return {
-    //       ...state,
-    //       employeeHappiness: state.employeeHappiness +4
-    //     }
-    //   },
-    // },
   ];
 
   const matchingEvent = fixedEvents.find((e) => e.condition(state));
