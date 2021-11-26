547d2b1ecd55fcb0df9377481cd9ce14a23b626d
Christopher Parsons <chris.p@madetech.com>
2021-11-26 15:13:09 +0000

    Set breaking-news image as default

--- src/contexts/selectedStoryImage.jsx
+++ src/contexts/selectedStoryImage.jsx
@@ -1,11 +1,12 @@
 import coffee from "../lib/images/coffee-beans.jpeg";
+import breaking from "../lib/images/story-breaking-news.png";
 
 const selectedStoryImage = (story) => {
   switch (story) {
     case "coffee":
       return <img style={{maxHeight: "200px"}} src={coffee} alt="coffee" />;
     default:
-      return <img style={{maxHeight: "200px"}} src={coffee} alt="coffee"/>;
+      return <img style={{maxHeight: "200px"}} src={breaking} alt="coffee"/>;
   }
 }
 
