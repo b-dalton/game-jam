5265b7814cf3eeaa206f013c7534a398c1d711e6
Chris Hutchinson <chris_hutchinson@me.com>
2021-11-26 15:27:59 +0000

    End game event

--- src/lib/fixed-event.js
+++ src/lib/fixed-event.js
@@ -1,3 +1,5 @@
+import { GameStageEnum } from "../contexts/GameState";
+
 export const getFixedEvent = (state) => {
   const fixedEvents = [
     {
@@ -14,6 +16,22 @@ export const getFixedEvent = (state) => {
         return {
           ...state,
           currency: state.currency + 420000,
+        };
+      },
+    },
+    {
+      name: `${state.companyName} becomes the greatest tech provider in the whole galaxy!`,
+      characterName: "@space-in-papers",
+      description: `${state.companyName} has beaten out their rival, Break Tech, to become the greatest tech provider in all of the galaxy. ${state.companyName}'s CEO said in a statement: "I'm ruddy thrilled I am, what a bloody win. I'm so proud of all my staff, but especially of myself, who somehow managed to make it through this entire game, I mean, simulation, I mean, real life experience... without losing". Arch rival CEO Roary (of the lion people planet Mufasa) said: "I'm devastated, roar. After everything we did to undermine their staff, their executive board, and their weird office parties, I can't believe they have become the best. You know what they say though, growl..., if you can't beat 'em, join em! I'm taking up a job as their new office gardener! Sayonara, purr".`,
+      type: "clackerNews",
+      condition: ({ gameTime }) => {
+        return gameTime >= 900;
+      },
+      joke: "u wot m8",
+      image: "coffee",
+      action: (state) => {
+        return {
+          ...state,
         };
       },
     },
